4302a4b7e5f59f78296e68ca96f10e168006e01d
nadalpeng000 <nadalpeng@gmail.com>
2023-05-01 12:10:32 +0800

    Initial Restaurant List Project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require packages used in project
+const exphbs = require('express-handlebars')
+
+// require template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// route setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
