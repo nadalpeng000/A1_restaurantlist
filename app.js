61a754e1d18a6ff778b31a5060ef57fdc3eae69b
nadalpeng000 <nadalpeng@gmail.com>
2023-05-01 14:59:45 +0800

    Update Restaurant List with search function

--- app.js
+++ app.js
@@ -17,9 +17,17 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
-app.get('/restaurants/:restaurant_id', (req,res) => {
+app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant})
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 // start and listen on the Express server
