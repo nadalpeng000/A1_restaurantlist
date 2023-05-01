e714618d33f75eb3dae7f89e2e6cbf56ed2ec7f8
nadalpeng000 <nadalpeng@gmail.com>
2023-05-01 13:56:06 +0800

    Add restaurant index from json list

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const port = 3000
 
 // require packages used in project
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // require template engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
@@ -14,7 +15,7 @@ app.use(express.static('public'))
 
 // route setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 // start and listen on the Express server
